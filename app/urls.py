4207ccf4fbbec8a403649a7b40f93a8ba1f3e28d
Bernardo <bernardobbm334@gmail.com>
2023-07-13 11:50:57 -0300

    feat: adição de novas urls (rotas) para a aplicação

--- app/urls.py
+++ app/urls.py
@@ -1,30 +1,18 @@
-"""
-URL configuration for app project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path
 # from account.views import register_view, login_view, logout_view
 from django.conf import settings
 from django.conf.urls.static import static
-from albuns.views import AlbunsListView
+from albuns.views import AlbunsListView, home_view, NewAlbumCreateView, AlbumDetailView, AlbumUpdateView, NewArtistCreateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home_view, name='home'),
     path('albuns/', AlbunsListView.as_view(), name='albuns_list'),
-    # path('new_album/', new_album_view, name='new_album'),
+    path('new_album/', NewAlbumCreateView.as_view(), name='new_album'),
+    path('new_artist/', NewArtistCreateView.as_view(), name='new_artist'),
+    path('album/<int:pk>/', AlbumDetailView.as_view(), name='album_detail'),
+    path('album/<int:pk>/update/', AlbumUpdateView.as_view(), name='album_update'),
     # path('register/', register_view, name='register'),
     # path('login/', login_view, name='login'),
     # path('logout/', logout_view, name='logout'),
