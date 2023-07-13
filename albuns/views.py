e9ca5ef5850ebeb452effeab2b612a49732e9cfa
Bernardo <bernardobbm334@gmail.com>
2023-07-13 11:48:39 -0300

    feat: adição de views para tratar das páginas de home e de formulários

--- albuns/views.py
+++ albuns/views.py
@@ -1,8 +1,12 @@
+from django.shortcuts import render
 from django.views.generic import ListView, CreateView, DetailView, UpdateView
 from django.utils.decorators import method_decorator
 from django.contrib.auth.decorators import login_required
-from albuns.models import Album
-from albuns.forms import AlbumModelForm
+from albuns.models import Album, Artist
+from albuns.forms import AlbumModelForm, ArtistModelForm
+
+def home_view(request):
+    return render(request, 'home.html')
 
 class AlbunsListView(ListView):
     model = Album
@@ -21,6 +25,12 @@ class NewAlbumCreateView(CreateView):
     model = Album
     form_class = AlbumModelForm
     template_name = 'new_album.html'
+    success_url = '/albuns/'
+
+class NewArtistCreateView(CreateView):
+    model = Artist
+    form_class = ArtistModelForm
+    template_name = 'new_artist.html'
     success_url = '/albuns/'
     
 
