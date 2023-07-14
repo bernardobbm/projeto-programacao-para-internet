be1ff6cf0f8488f37eb6a0a5676ec94e39b38e74
Bernardo <bernardobbm334@gmail.com>
2023-07-13 21:41:23 -0300

    fix: correção de códigos responsaveis pela pagina inicial, bem como o template base e adição de urls (rotas)

--- app/urls.py
+++ app/urls.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
 from django.urls import path
-# from account.views import register_view, login_view, logout_view
 from django.conf import settings
 from django.conf.urls.static import static
-from albuns.views import AlbunsListView, home_view, NewAlbumCreateView, AlbumDetailView, AlbumUpdateView, NewArtistCreateView
+from albuns.views import AlbunsListView, home_view, NewAlbumCreateView, AlbumDetailView, AlbumDeleteView, AlbumUpdateView, NewArtistCreateView
+from accounts.views import login_view, register_view, logout_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -12,8 +12,9 @@ urlpatterns = [
     path('new_album/', NewAlbumCreateView.as_view(), name='new_album'),
     path('new_artist/', NewArtistCreateView.as_view(), name='new_artist'),
     path('album/<int:pk>/', AlbumDetailView.as_view(), name='album_detail'),
+    path('album/<int:pk>/delete/', AlbumDeleteView.as_view(), name='album_delete'),
     path('album/<int:pk>/update/', AlbumUpdateView.as_view(), name='album_update'),
-    # path('register/', register_view, name='register'),
-    # path('login/', login_view, name='login'),
-    # path('logout/', logout_view, name='logout'),
+    path('login/', login_view, name='login'),
+    path('logout/', logout_view, name='logout'),
+    path('register/', register_view, name='register'),
 ] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
